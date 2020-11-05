ffe9517c1e51410324176de1af5220c055654c3f
Xiaohan Yin <xyin13@MacBook-Pro.local>
2020-11-04 23:38:44 -0600

    updated shop page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
+
 import './App.css';
+import { Switch, Route} from 'react-router-dom';
+
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
