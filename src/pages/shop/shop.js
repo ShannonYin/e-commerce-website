974b828142e40fe83cae86714650283be39b27e8
Xiaohan Yin <xyin13@MacBook-Pro.local>
2020-12-12 22:55:46 -0600

    refactor the shop page

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,28 +1,10 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/preview-collection/preview-collection';
+import CollectionsOverview from '../../components/collections-overview/collections-overview';
 
-class ShopPage extends React.Component{
-    constructor(){
-        super();
-
-        this.state = {
-            collections: SHOP_DATA
-        };
-    }
-
-    render(){
-        const {collections} = this.state;
-        return (
-            <div className='shop-page'>
-                {
-                    collections.map(({id, ...otherCollectionProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionProps}/>
-                    ))
-                }
-            </div>
-        )
-    }
-}
+const ShopPage = ({collections}) => (
+    <div className='shop-page'>
+        <CollectionsOverview/>
+    </div>
+);
 
 export default ShopPage;
