eabdd8b69b333f34a9ca84b611c0db6f5012b1f8
Xiaohan Yin <xyin13@MacBook-Pro.local>
2020-12-13 19:19:24 -0600

    integrate stripe in the app

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selector';
 import CheckoutItem from '../../components/checkout-item/checkout-item';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button';
+
 const CheckoutPage = ({cartItems, total}) => (
     <div className='checkout-page'>
         <div className='checkout-header'>
@@ -26,9 +28,13 @@ const CheckoutPage = ({cartItems, total}) => (
         {
             cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem}/>)
         }
-        <div className='total'>
-            <span>TOTAL: ${total}</span>
+        <div className='total'>TOTAL: ${total}</div>
+        <div className='test-warning'>
+            *Please use the following test credit card for payments*
+            <br />
+            4242 4242 4242 4242 - Exp: 01/24 - CVV: 123
         </div>
+        <StripeCheckoutButton price={total}/>
     </div>
 );
 
