7251e3865d181175c298fc59f5b5a1d553641382
Xiaohan Yin <xyin13@MacBook-Pro.local>
2020-12-25 21:35:22 -0600

    update the styles

--- client/src/components/collection-item/collection-item.js
+++ client/src/components/collection-item/collection-item.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import CustomButton from '../custom-button/custom-button';
 import { connect } from 'react-redux';
 import { addItem } from '../../redux/cart/cart.actions';
-import './collection-item.scss';
-
+import { CollectionItemContainer, AddButton, BackgroundImage, CollectionFooterContainer, NameContainer, PriceContainer} from './collection-item.styles';
 const CollectionItem = ({item, addItem}) => {
     const { name, price, imageUrl} = item;
     return (
-        <div className='collection-item'>
-            <div 
-                className='image'
+        <CollectionItemContainer>
+            <BackgroundImage
                 style={{
                     backgroundImage: `url(${imageUrl})`
                 }}
             />
-            <div className='collection-footer'>
-                <span className='name'>{name}</span>
-                <span className='price'>{price}</span>
-            </div>
-            <CustomButton onClick = {() => addItem(item)} inverted>Add to cart</CustomButton>
-        </div>
+            <CollectionFooterContainer>
+                <NameContainer>{name}</NameContainer>
+                <PriceContainer>{price}</PriceContainer>
+            </CollectionFooterContainer>
+            <AddButton onClick = {() => addItem(item)} inverted>Add to cart</AddButton>
+        </CollectionItemContainer>
     )
 }
 
